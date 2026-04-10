1203ad569c98a3c46ee24bc09a0fdc69574cbd6d
dim-ghub <59436063+dim-ghub@users.noreply.github.com>
2026-04-10 01:32:19 -0500

    Change echo to printf for install message

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,4 +12,4 @@ printf "Cloning artix-installer...\n"
 sudo pacman -Sy --needed --noconfirm git
 git clone --depth=1 "$REPO_URL" "$CLONE_DIR"
 cd "$CLONE_DIR"
-echo "Run ./install.sh"
+printf "Run ./install.sh"
