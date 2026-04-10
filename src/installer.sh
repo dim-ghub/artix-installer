5bed44923d44285c595bd6fe1bd66af5c2e89358
dim-ghub <mlrudasill@gmail.com>
2026-04-10 01:34:10 -0500

    Remove non-existent sysvinit package

--- src/installer.sh
+++ src/installer.sh
@@ -19,7 +19,7 @@
 # You should have received a copy of the GNU General Public License
 # along with artix-installer. If not, see <https://www.gnu.org/licenses/>.
 
-pkgs="base base-devel $MY_INIT sysvinit elogind-$MY_INIT efibootmgr limine btrfs-progs wget dhcpcd wpa_supplicant connman-$MY_INIT hyprland kitty nano sddm sddm-openrc"
+pkgs="base base-devel $MY_INIT elogind-$MY_INIT efibootmgr limine btrfs-progs wget dhcpcd wpa_supplicant connman-$MY_INIT hyprland kitty nano sddm sddm-openrc"
 [ "$ENCRYPTED" = "y" ] && pkgs="$pkgs cryptsetup cryptsetup-$MY_INIT"
 
 # Partition disk
